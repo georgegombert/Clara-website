a385b04120f3af7e37ab9c4e3c1687e4235f783a
george-g <georgegombert@gmail.com>
2020-04-10 16:44:05 -0600

    Added lazy scrolling for touch. Started fade styling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,33 @@
-const mobileNav = document.querySelector("#mobileNav");
+const mobileNav = $("#mobileNav");
+const navHome = $("#navHome");
+const navWriting = $("#navWriting");
+const navArt = $("#navArt");
+const navAbout = $("#navAbout");
+const navContact = $("#navContact");
+const navItems = $(".mobile-nav-item");
 
-mobileNav.addEventListener("scroll", () => {
+mobileNav.scroll( () => {
     // scrollValue = mobileNav.getBoundingClientRect();
-    console.log(mobileNav.scrollTop);
+    homePosition = navHome.position().top-495;
+    writingPosition = navWriting.position().top-479;
+    console.log("Scroll: "+mobileNav.scrollTop()+"");
+    console.log("Home: "+(navHome.position().top-495)+"");
+    console.log("writing: "+(navWriting.position().top-479)+"");
+    console.log("Art: "+(navArt.position().top-495)+"");
+    console.log("Contact: "+(navContact.position().top-495)+"");
+    if(homePosition < -20){
+        navHome.fadeTo(0,(1-(mobileNav.scrollTop()/70)));
+    } 
+    // if(navWriting < 70 && navWriting > 10){
+    //     navWriting.fadeTo()
+    // }
+    else{
+        navItems.fadeTo(0,1);
+    }
+
 });
+
+// MEDIAN SCROLL POSITION 495 navArt.position().top
+// TOP SCROLL POSITION 405  mobileNav.scrollTop()
+// BOTTOM SCROLL POSITION 551
+// SCROLL AMOUNT 228
